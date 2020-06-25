5a42c446f0512866f4bbadb00e3f20f318915773
zacktu <member@neqn.net>
2020-06-25 10:06:03 -0400

    Exercise 1.17 f-strings

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,6 +12,7 @@ extra_payment = 1000
 extra_payment_start = 13
 extra_payment_end = 24
 
+print ("Payment  Amount Paid    Total Paid    Remaining")
 while principal > 0:
     num_payments += 1
     if (num_payments >= extra_payment_start) and (num_payments <= extra_payment_end):
@@ -25,7 +26,7 @@ while principal > 0:
         principal = principal * rate_multiplier - this_payment
 
     total_paid = total_paid + this_payment
-    print(num_payments, round(this_payment, 2), round(total_paid, 2), round(principal, 2))
+    print(f'    {num_payments:2}     {this_payment:5.2f}     {total_paid:10.2f}      {principal:8.2f}')
 
 print()
 print ('Total paid = ', round(total_paid, 2))
