ebbd40b91264e0dfab5ce6d7be3548e3a4bde023
zacktu <member@neqn.net>
2020-06-26 12:13:25 -0400

    Exercise 1.27 Reading a data file more compact version

--- Work/Data/pcost.py
+++ Work/Data/pcost.py
@@ -4,13 +4,8 @@ headers = next(f).split(',')
 print(headers)
 total_price = 0.0
 for line in f:
-    #print(line, end=' ')
     linelist = line.split(',')
-    #print(linelist)
-    shares = linelist[1]
-    price = linelist[2]
-    purchase_price = int(shares) * float(price)
-    print(shares, price, purchase_price)
-    total_price += purchase_price
-print("Total price = ", total_price)
+    print(linelist)
+    total_price += int(linelist[1]) * float(linelist[2])
+print("Total cost = ", total_price)
 f.close()
