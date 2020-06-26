93165011b56f0f1c67596a85ff16ae77c01cb5d9
zacktu <member@neqn.net>
2020-06-26 11:31:00 -0400

    Exercise 1.26 File Preliminaries using next

--- Work/Data/filemanagement.py
+++ Work/Data/filemanagement.py
@@ -2,6 +2,10 @@ import os
 mydir = os.getcwd()
 #print(mydir)
 
-with open('portfolio.csv', 'rt') as f:
-    for line in f:
-        print (line, end = '')
+f = open('portfolio.csv', 'rt')
+headers = next(f).split(',')
+print(headers)
+for line in f:
+    row = line.split(',')
+    print(row)
+f.close()
