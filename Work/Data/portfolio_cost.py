54dd6874a41657b4912bbf27f61535ec08060f31
zacktu <member@neqn.net>
2020-06-27 11:03:40 -0400

    Section 1.7 Exercise 1.30 Turning a script into a function changed open to use with

--- Work/Data/portfolio_cost.py
+++ Work/Data/portfolio_cost.py
@@ -1,16 +1,13 @@
 # portfolio_cost.py
 
 def portfolio_cost():
-    f = open('portfolio.csv', 'rt')
-    headers = next(f).split(',')
-    total_price = 0.0
-    for line in f:
-        linelist = line.split(',')
-        total_price += int(linelist[1]) * float(linelist[2])
-    f.close()
-    return total_price
+    with open('portfolio.csv', 'rt') as f:
+        headers = next(f).split(',')
+        total_price = 0.0
+        for line in f:
+            linelist = line.split(',')
+            total_price += int(linelist[1]) * float(linelist[2])
+        return total_price
 
-print('about to invoke portfolio_cost function')
 cost = portfolio_cost()
-print('returned from portfolio_cost function')
 print('Total cost = ', cost)
