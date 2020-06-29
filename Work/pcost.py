caf8acdbb2ae3f0454af36564d7553146cdd6e9d
zacktu <member@neqn.net>
2020-06-29 14:23:24 -0400

    Exercise 1.33 Reading from the command line Moved to Work directory.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,23 @@
 # pcost.py
-#
-# Exercise 1.27
+
+import sys
+
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        total_price = 0.0
+        for line in f:
+            linelist = line.split(',')
+            try:
+                total_price += int(linelist[1]) * float(linelist[2])
+            except ValueError:
+                print ('Error in line: ', line)
+        return total_price
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    cost = portfolio_cost(filename)
+else:
+    cost = portfolio_cost('Data/portfolio.csv')
+
+print('Total cost = ', cost)
