69b081f2ce77df0cdf9269a076bf71612755ae94
zacktu <member@neqn.net>
2020-07-01 09:52:29 -0400

    Exercise 1.33 Reading from the command line Copied from the solutions set in attempt to solve problem with reading from Data directory.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,23 +1,31 @@
 # pcost.py
 
-import sys
-
+import csv
 def portfolio_cost(filename):
-    with open(filename, 'rt') as f:
-        headers = next(f).split(',')
-        total_price = 0.0
-        for line in f:
-            linelist = line.split(',')
-            try:
-                total_price += int(linelist[1]) * float(linelist[2])
-            except ValueError:
-                print ('Error in line: ', line)
-        return total_price
+    '''
+    Computes the total cost (shares*price) of a portfolio file
+    '''
+    total_cost = 0.0
 
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print('Bad row:', row)
+
+    return total_cost
+
+import sys
 if len(sys.argv) == 2:
     filename = sys.argv[1]
-    cost = portfolio_cost(filename)
 else:
-    cost = portfolio_cost('Data/portfolio.csv')
+    filename = input('Enter a filename:')
 
-print('Total cost = ', cost)
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
