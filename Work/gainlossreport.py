66d513caa6d4c53e76116b6e0e687f4dfe2a8898
zacktu <member@neqn.net>
2020-07-07 10:45:54 -0400

    Section 2.2 Containers Exercise 2.4 A list of tuples Added line space before printing total value. Exercise 2.5 List of Dictionaries Cleaned up a little bit by removing unnecessary assignments. Exercise 2.6 Dictionaries as a container. Renamed read_prices as current_prices. Can read from prices.csv into current_prices dictionary.

---
+++ Work/gainlossreport.py
@@ -0,0 +1,55 @@
+# gainlossreport.py
+#
+# Exercise 2.7 Dictionaries as a container
+
+import sys
+import csv
+
+def current_prices(filename):
+    """
+    Build the prices dictionary
+    """
+
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                next_row = {row[0]:row[1]}
+                prices.update(next_row)
+            except IndexError:
+                pass
+
+    return prices
+
+
+# for quick testing to avoid typing
+#filename = 'Data/prices.csv'
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+stock_prices = current_prices(filename)
+print('stock_prices = ', stock_prices)
+print('Price of MSFT is ', stock_prices['MSFT'])
+print('Price of BAC is ', stock_prices['BAC'])
+print('Price of MMM is ', stock_prices['MMM'])
+
+print("\nThat's all folks!")
+# for quick testing to avoid typing
+#filename = 'Data/prices.csv'
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+stock_prices = current_prices(filename)
+print('stock_prices = ', stock_prices)
+print('Price of MSFT is ', stock_prices['MSFT'])
+print('Price of BAC is ', stock_prices['BAC'])
+print('Price of MMM is ', stock_prices['MMM'])
+
+print("\nThat's all folks!")
--- Work/report.py
+++ Work/report.py
@@ -1,40 +1,83 @@
 # report.py
 #
-# Exercise 2.7 Dictionaries as a container
+# Exercise 2.7 Computer gain and loss for portfolio
 
-import sys
 import csv
 
-def current_prices(filename):
+def getcurrentprices(currentportfolio):
     """
     Build the prices dictionary
     """
 
-    prices = {}
-    with open(filename, 'rt') as f:
+    current_prices = {}
+    with open(currentportfolio, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
             try:
-                next_row = {row[0]:row[1]}
-                prices.update(next_row)
+                nextrow = {row[0]:row[1]}
+                #print('nextrow = ', nextrow)
+                current_prices.update(nextrow)
+                #print('current_prices = ', current_prices)
             except IndexError:
                 pass
+        #print('current_prices = ', current_prices)
+    return current_prices
 
-    return prices
+def getoriginalportfolio(originalholdings):
+
+    """
+    Build the portfolio
+    """
+
+    print('Entering getoriginalportfolio with files:', originalholdings)
+
+    portfolio = []
+
+    with open(originalholdings, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                dict = {headers[0]:row[0], headers[1]:row[1], headers[2]: row[2]}
+                portfolio.append(dict)
+            except ValueError:
+                print('Bad row:', row)
+        #print ('Portfolio = ', portfolio)
+        return portfolio
 
 # for quick testing to avoid typing
-#filename = 'Data/prices.csv'
+currentholdingsfilename = 'Data/prices.csv'
+originalholdingsfilename = 'Data/portfolio.csv'
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = input('Enter a filename:')
+#original_portfolio = getoriginalportfolio(originalholdingsfilename)
+#print('ORIGINAL PORTFOLIO =', original_portfolio)
+current_prices = getcurrentprices(currentholdingsfilename)
+print('CURRENT PRICES =', current_prices)
+print('CURRENT PRICE OF IBM IS', current_prices['IBM'])
 
-stock_prices = current_prices(filename)
-print('stock_prices = ', stock_prices)
-print('Price of MSFT is ', stock_prices['MSFT'])
-print('Price of BAC is ', stock_prices['BAC'])
-print('Price of MMM is ', stock_prices['MMM'])
+# # print('ORIGINAL PORTFOLIO')
+# # for stock in original_portfolio:
+# #     print(stock['name'], stock['shares'], stock['price'])
+#
+# print('CURRENT PORTFOLIO')
+# for current_stock in original_portfolio:
+#     print(current_stock['name'], current_stock['price'])
+
+#name = 'AA'
+#print(current_prices['AA'])
+
+#print('Current price of AA is:', current_prices['AA'])
+
+# if len(sys.argv) == 2:
+#     filename = sys.argv[1]
+# else:
+#     filename = input('Enter a filename:')
+#
+# stock_prices = current_prices(filename)
+# print('stock_prices = ', stock_prices)
+# print('Price of MSFT is ', stock_prices['MSFT'])
+# print('Price of BAC is ', stock_prices['BAC'])
+# print('Price of MMM is ', stock_prices['MMM'])
 
 print("\nThat's all folks!")
 
