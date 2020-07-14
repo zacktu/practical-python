071043e768c422aa8ea86117bcb64201de084a03
zacktu <member@neqn.net>
2020-07-14 16:34:07 -0400

    Section 2.2 Containers Exercise 2.4 A list of tuples Added line space before printing total value. Exercise 2.5 List of Dictionaries Cleaned up a little bit by removing unnecessary assignments. Exercise 2.6 Dictionaries as a container. Renamed read_prices as current_prices. Can read from prices.csv into current_prices dictionary. Can read from portfolio.csv into original_portfolio list. Can compute profit/loss.  Need to improve code. Took out lots of intermediate variables  Need to change the way the filenames are entered, but probably won't do it.  Maybe also echo some of the data. Added some statements to make the program more of a report. Exercise 2.9 Collecting Data Added new function make_report -- it was easy because I already had functions getcurrentprices and getoriginalportfolio. Exercise 2.10 Print a formatted table.  Found a way to format that I like and will probably continue to use. Exercise 2.11 Adding some headers. Exercise 2.12 Formatting Challenge Exercise 2.17 Using the zip() function. So far I'm reading the file portfolio.csv with a hardwired file invocation.  Will change to request a filename and use a different file.

--- Work/pcost.py
+++ Work/pcost.py
@@ -11,22 +11,29 @@ def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
+
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                nshares = int(row[1])
-                price = float(row[2])
-                total_cost += nshares * price
+                print(record['name'], record['shares'], record['price'])
+                numshares = int(record['shares'])
+                shareprice = float(record['price'])
+                total_cost += numshares * shareprice
             # This catches errors in int() and float() conversions above
             except ValueError:
-                print('Bad row:', row)
+                print(f'Row {rowno}: Bad row: {row}')
 
     return total_cost
 
+''' Use hardwired filename instead of prompt
 import sys
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = input('Enter a filename:')
+'''
+
+filename = 'Data/portfolio.csv'
 
 cost = portfolio_cost(filename)
-print('Total cost:', cost)
+print('\nTotal cost:', cost)
