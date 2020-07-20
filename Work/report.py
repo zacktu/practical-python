dc1e329d8871bca93c386d2b801b2131c3572721
zacktu <member@neqn.net>
2020-07-20 16:13:49 -0400

    Section 2.2 Containers Exercise 2.4 A list of tuples Added line space before printing total value. Exercise 2.5 List of Dictionaries Cleaned up a little bit by removing unnecessary assignments. Exercise 2.6 Dictionaries as a container. Renamed read_prices as current_prices. Can read from prices.csv into current_prices dictionary. Can read from portfolio.csv into original_portfolio list. Can compute profit/loss.  Need to improve code. Took out lots of intermediate variables  Need to change the way the filenames are entered, but probably won't do it.  Maybe also echo some of the data. Added some statements to make the program more of a report. Exercise 2.9 Collecting Data Added new function make_report -- it was easy because I already had functions getcurrentprices and getoriginalportfolio. Exercise 2.10 Print a formatted table.  Found a way to format that I like and will probably continue to use. Exercise 2.11 Adding some headers. Exercise 2.12 Formatting Challenge Exercise 2.17 Using the zip() function. So far I'm reading the file portfolio.csv with a hardwired file invocation.  Will change to request a filename and use a different file. Exercise 2.17 Again.  Changed from hardwired filename to reading the filename.  Also tested with portfoliodate.csv as well as portfolio.csv.  It worked because the first row contained names of all the columns. Exercise 2.17 Yet again.  Used the zip and enumerate method from pcost.py in report.py. Exercise 2.17 once more -- removed a collection of debugging statements that I had neglected to remove before. Exercise 2.17 Removed redundant code. Exercise 2.17 Renamed functions to variables to comply with Python coding suggestions. Exercise 3.2 Creating a top-level function.  I've made the top-level function call lower-level functions.  Still need to run the program with command-line arguments.

--- Work/report.py
+++ Work/report.py
@@ -22,7 +22,6 @@ def get_current_prices(current_portfolio):
     return current_prices
 
 def get_original_portfolio(original_holdings):
-
     """
     Build the portfolio
     """
@@ -44,7 +43,7 @@ def get_original_portfolio(original_holdings):
         #sys.exit()
         return portfolio
 
-def make_report(current_prices, original_portfolio):
+def build_stocklist(current_prices, original_portfolio):
     stocklist = []
     for stock in original_portfolio:
         shares = int(stock['shares'])
@@ -54,11 +53,86 @@ def make_report(current_prices, original_portfolio):
         stocklist.append([stock['name'], shares, current_price, gainloss])
     return stocklist
 
-# for quick testing to avoid typing
-#current_holdings_filename = 'Data/prices.csv'
-#original_holdings_filename = 'Data/portfolio.csv'
+def print_report(stocklist):
+    '''
+    :format and print the stock report
+    '''
 
-#stock_prices = current_prices(filename)
+
+# Computer gain and loss for portfolii report.py
+#
+# Exercise 2.12 Formatting Challenge
+
+import sys
+import csv
+
+
+def get_current_prices(current_portfolio):
+    """
+    Build the prices dictionary
+    """
+
+    current_prices = {}
+    with open(current_portfolio, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                nextrow = {row[0]: row[1]}
+                current_prices.update(nextrow)
+            except IndexError:
+                pass
+    return current_prices
+
+
+def get_original_portfolio(original_holdings):
+    """
+    Build the portfolio
+    """
+
+    portfolio = []
+
+    with open(original_holdings, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for rowno, row in enumerate(rows, start=1):
+            try:
+                record = dict(zip(headers, row))
+                portfolio.append(record)
+            except ValueError:
+                print('Bad row:', row)
+
+        # print('THats all for now!')
+        # sys.exit()
+        return portfolio
+
+
+def build_stocklist(current_prices, original_portfolio):
+    stocklist = []
+    for stock in original_portfolio:
+        shares = int(stock['shares'])
+        original_price = float(stock['price'])
+        current_price = float(current_prices[stock['name']])
+        gainloss = float(current_price - original_price)
+        stocklist.append([stock['name'], shares, current_price, gainloss])
+    return stocklist
+
+def print_report(stocklist):
+    '''
+    :format and print the stock report
+    '''
+    print('      Name     Shares      Price     Change')
+    print('---------- ---------- ---------- ----------')
+    for name, shares, price, change in stocklist:
+        dollarprice = '$' + str(price)
+        print(f'{name:>10s} {shares:>10d} {dollarprice:>10s} {change:>10.2f}')
+
+
+# for quick testing to avoid typing
+# current_holdings_filename = 'Data/prices.csv'
+# original_holdings_filename = 'Data/portfolio.csv'
+
+# stock_prices = current_prices(filename)
 
 # if len(sys.argv) == 2:
 #     filename = sys.argv[1]
@@ -66,18 +140,17 @@ def make_report(current_prices, original_portfolio):
 #     filename = input('Enter a filename:')
 
 original_holdings_filename = input('Enter name of portfolio file:')
-original_portfolio = get_original_portfolio(original_holdings
-
+original_portfolio = get_original_portfolio(original_holdings_filename)
 current_holdings_filename = input('Enter name of current stock prices file:')
 current_prices = get_current_prices(current_holdings_filename)
-
-stocklist = make_report(current_prices, original_portfolio)
-print('      Name     Shares      Price     Change')
-print('---------- ---------- ---------- ----------')
-for name, shares, price, change in stocklist:
-    dollarprice = '$' + str(price)
-    print(f'{name:>10s} {shares:>10d} {dollarprice:>10s} {change:>10.2f}')
-
+stocklist = build_stocklist(current_prices, original_portfolio)
+print_report(stocklist)
 print("\nThat's all folks!")
+
+# if len(sys.argv) == 2:
+#     filename = sys.argv[1]
+# else:
+#     filename = input('Enter a filename:')
+
 
 
