4034f92b90ebc5f17877b5035624aa4606408bbb
zacktu <member@neqn.net>
2020-07-27 09:57:40 -0400

    Section 2.2 Containers Exercise 2.4 A list of tuples Added line space before printing total value. Exercise 2.5 List of Dictionaries Cleaned up a little bit by removing unnecessary assignments. Exercise 2.6 Dictionaries as a container. Renamed read_prices as current_prices. Can read from prices.csv into current_prices dictionary. Can read from portfolio.csv into original_portfolio list. Can compute profit/loss.  Need to improve code. Took out lots of intermediate variables  Need to change the way the filenames are entered, but probably won't do it.  Maybe also echo some of the data. Added some statements to make the program more of a report. Exercise 2.9 Collecting Data Added new function make_report -- it was easy because I already had functions getcurrentprices and getoriginalportfolio. Exercise 2.10 Print a formatted table.  Found a way to format that I like and will probably continue to use. Exercise 2.11 Adding some headers. Exercise 2.12 Formatting Challenge Exercise 2.17 Using the zip() function. So far I'm reading the file portfolio.csv with a hardwired file invocation.  Will change to request a filename and use a different file. Exercise 2.17 Again.  Changed from hardwired filename to reading the filename.  Also tested with portfoliodate.csv as well as portfolio.csv.  It worked because the first row contained names of all the columns. Exercise 2.17 Yet again.  Used the zip and enumerate method from pcost.py in report.py. Exercise 2.17 once more -- removed a collection of debugging statements that I had neglected to remove before. Exercise 2.17 Removed redundant code. Exercise 2.17 Renamed functions to variables to comply with Python coding suggestions. Exercise 3.2 Creating a top-level function.  I've made the top-level function call lower-level functions.  Still need to run the program with command-line arguments. Exercise 3.2 Can now run program with command line arguments or by reading command line inputs. Exercise 3.4 Building a Column Selector Exercise 3.5 Performing Type Conversion Exercise 3.6 Working Without Headers -- added has_headers parameter and made code to return portfolio as selected rows from the file instead of a dictionary -- the types parameter still enables setting the types of the values returned from the csv file Exercise 3.7 Picking a different column delimiter.  This adds an additional parameter, delimiter, that is the delimiter for a "CSV" file, only it delimited by a character other than a comma. Exercise 3.8 Raising exeptions included a test for when the file has no headers and the select parameter provides names of columns.  Raises a RunTimeError based upon values of parameters. Exercise 3.8 again removed some print statements and included proper usage of "raise". Exercise 3.9 Catching Exceptions.  Catch exception when a data value is missing.  Good exercise in multiple use of try-catch to get down to specific location where exception might occur.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,39 +1,49 @@
 # fileparse.py
 #
-# Section 3.3 Error Checking
-#
-# Exercise 3.7 Picking a different column delimiter
+# Exercise 3.9 Catching Exceptions
 
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=''):
+def parse_csv(filename, select=None, types=None,
+                    has_headers=True, delimiter=''):
     '''
     Parse a CSV file into a list of records
     '''
     try:
         with open(filename) as f:
             if delimiter == '':
+                rowno = 1
                 rows = csv.reader(f)
             else:
+                rowno = 1
                 rows = csv.reader(f, delimiter=delimiter)
 
             if not has_headers:
                 records = []
                 for row in rows:
+                    rowno+= 1
                     if not row:    # Skip rows with no data
                         continue
                     if select:
-                        raise RuntimeError("select argument requires column headers")
+                        raise RuntimeError\
+                                ("select argument requires column headers")
                     if types:
-                        row = [func(val) for func, val in zip(types, row)]
-                    records.append(row)
+                        try:
+                            row = [func(val) for func, val in zip(types,
+                                                                  row)]
+                            records.append(row)
+                        except ValueError as e:
+                            print('ERROR in row number', rowno, ': ', e)
             else:
                 # Read the file headers
+                rowno = 1
                 headers = next(rows)
 
-                # If a column selector was given, find indices of the specified columns.
-                # Also narrow the set of headers used for resulting dictionaries
+                # If a column selector was given, find indices
+                #       of the specified columns.
+                # Also narrow the set of headers used for
+                #       resulting dictionaries
                 if select:
                     indices = [headers.index(colname) for colname in select]
                     headers = select
@@ -42,18 +52,23 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter='')
 
                 records = []
                 for row in rows:
+                    rowno += 1
                     if not row:    # Skip rows with no data
                         continue
                     # Filter the row if specific columns were selected
                     if indices:
                         row = [row[index] for index in indices ]
                     if types:
-                        row = [func(val) for func, val in zip(types, row)]
-
+                        try:
+                            row = [func(val) for func, val in
+                                   zip(types, row)]
+                        except ValueError as e:
+                            print('Error in row number', rowno, ': ', e)
                     # Make a dictionary
                     record = dict(zip(headers, row))
                     records.append(record)
 
             return records
     except RuntimeError as e:
-        print('ERROR" ', e)
+        print('ERROR: ', e)
+
